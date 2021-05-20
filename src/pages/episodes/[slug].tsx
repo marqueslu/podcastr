655abf58816c9b5ad5f9e8a98254d9607410030f
Lucas Marques <lucas.msilva09@gmail.com>
2021-05-20 09:25:44 -0300

    Add play function in episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -9,6 +9,7 @@ import { api } from '../../services/api'
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
 
 import styles from './episode.module.scss'
+import { usePlayer } from '../../contexts/PlayerContext'
 
 type Episode = {
   id: string
@@ -28,6 +29,7 @@ type EpisodeProps = {
 
 export default function Episode({ episode }: EpisodeProps) {
   const router = useRouter()
+  const { play } = usePlayer()
 
   if (router.isFallback) {
     return <p>Carregando....</p>
@@ -47,7 +49,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
